0f08b05d0eace5a57f7c06be20ce6482ded8fa06
Ashkan Selahvarzy <ashkan.slvz@outlook.com>
2025-09-22 09:42:42 +0330

    Fixed a bug and update DBcontext

--- Components/LogicAndModel/LinkModel.cs
+++ Components/LogicAndModel/LinkModel.cs
@@ -2,9 +2,9 @@
 
 public class LinkModel
 {
-    public string? ID { get; set; }
-    public string? Url { get; set; }
-    public string? Qr { get; set; }
-    public string? Date { get; set; }
+    public string? ID { get; set; } = "";
+    public string? Url { get; set; } = "";
+    public string? Qr { get; set; } = "";
+    public string? Date { get; set; } = "";
     public Int64 Seen { get; set; } = 0;
 }
